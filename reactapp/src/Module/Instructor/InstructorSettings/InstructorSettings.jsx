54f183926c6d3ee497804bd6203a9aa874dc201c
Sabari055 <sabari21032002@gmail.com>
2023-07-11 15:37:32 +0000

    edited

---
+++ reactapp/src/Module/Instructor/InstructorSettings/InstructorSettings.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./InstructorSettings.css"
+
+function InstructorSettings() {
+  return (
+    <div className='start'>InstructorSettingsssssssssssssssssssssssssssssssssssssssssss</div>
+  )
+}
+
+export default InstructorSettings
---
+++ springapp/src/main/java/com/example/learningportal/repository/UserInfoRepository.java
@@ -0,0 +1,14 @@
+package com.example.learningportal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.learningportal.entity.UserInfo;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer>  {
+	
+	 Optional<UserInfo> findByName(String username);
+
+}
+
