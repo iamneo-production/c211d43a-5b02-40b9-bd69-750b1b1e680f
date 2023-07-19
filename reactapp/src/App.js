3084d1bc28d763d802b71e6418770a5c604e9d90
Aiswarya1212 <aiswaryasivakumar84@gmail.com>
2023-07-19 14:11:40 +0000

    Leaderboard

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Leaderboard from "./components/Leaderboard";
+
+function App() {
+  return (
+    <div>
+      
+    
+      <Leaderboard/>
+    
+    </div>
+  );
+}
+
+export default App;
