4bf6cb6caa44568a541c5b630deb9b6b3a5b628f
Rahulkumar246810 <rahulkumar246810@gmail.com>
2023-07-20 13:48:24 +0000

    update

--- springapp/src/main/java/com/example/springapp/controller/InstructorController.java
+++ springapp/src/main/java/com/example/springapp/controller/InstructorController.java
@@ -18,41 +18,44 @@ import com.example.springapp.model.Admin;
 import com.example.springapp.services.InstructorService;
 
 @RestController
-// @CrossOrigin(origins = "http://localhost:3000")
 public class InstructorController {
-	
-	@Autowired
-	private InstructorService instructorService;
-	
-	@GetMapping("/admins")
-	public List<Admin> getAdmins(){
-		return this.instructorService.getAdmins();
-	}
-	
-	@GetMapping("/admins/{adminId}")
-	public Admin getAdmin(@PathVariable String adminId) {
-		return this.instructorService.getAdmin(Long.parseLong(adminId));
-	}
-	
-	@PostMapping("/admins")
-	public Admin addAdmin(@RequestBody Admin admin) {
-		return this.instructorService.addAdmin(admin);
-	}
-	
-	@PutMapping("/admins/{adminId}")
-	public Admin updateAdmin(@PathVariable String adminId, @RequestBody Admin admin) {
-		admin.setId(Long.parseLong(adminId));
-		return this.instructorService.updateAdmin(admin);
-	}
-	
-	@DeleteMapping("/admins/{adminId}")
-	public ResponseEntity<HttpStatus> deleteAdmin(@PathVariable String adminId){
-		try {
-			this.instructorService.deleteAdmin(Long.parseLong(adminId));
-			return new ResponseEntity<>(HttpStatus.OK);
-		}catch (Exception e) {
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-}
 
+    @Autowired
+    private InstructorService instructorService;
+
+    @GetMapping("/home")
+    public String home() {
+        return "this is home page";
+    }
+
+    @GetMapping("/admins")
+    public List<Admin> getAdmins() {
+        return this.instructorService.getAdmins();
+    }
+
+    @GetMapping("/admins/{adminId}")
+    public Admin getAdmin(@PathVariable String adminId) {
+        return this.instructorService.getAdmin(Long.parseLong(adminId));
+    }
+
+    @PostMapping("/admins")
+    public Admin addAdmin(@RequestBody Admin admin) {
+        return this.instructorService.addAdmin(admin);
+    }
+
+    @PutMapping("/admins/{adminId}")
+    public Admin updateAdmin(@PathVariable String adminId, @RequestBody Admin admin) {
+        admin.setId(Long.parseLong(adminId));
+        return this.instructorService.updateAdmin(admin);
+    }
+
+    @DeleteMapping("/admins/{adminId}")
+    public ResponseEntity<HttpStatus> deleteAdmin(@PathVariable String adminId) {
+        try {
+            this.instructorService.deleteAdmin(Long.parseLong(adminId));
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
