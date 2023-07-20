8db67b2f9fdeb486e759563d98ba4322b378361b
Rahulkumar246810 <rahulkumar246810@gmail.com>
2023-07-20 06:46:08 +0000

    update

--- reactapp/src/Components/Lesson/Video.jsx
+++ reactapp/src/Components/Lesson/Video.jsx
@@ -5,13 +5,15 @@ import React from "react";
 export default function Video(props) {
   return (
     <React.Fragment>
-      <video
+      {/* <video
         className="page-width border_radius"
         // src={video}
+        src="https://drive.google.com/file/d/15-LE8k855nl_9Tnub36dlKmHqmxU6_jV/view?usp=sharing"
         controls
-        // autoPlay
-        // poster={Thumbnail}
-      />
+        autoPlay
+        poster={Thumbnail}
+      /> */}
+      <iframe className="page-width border_radius" style={{height:"85vh"}} src="https://www.youtube.com/embed/GdAon80-0KA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </React.Fragment>
   );
 }
