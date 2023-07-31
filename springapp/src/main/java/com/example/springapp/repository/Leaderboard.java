18aba2a70bf31c279f913f462c93057d9c23f7a9
Aiswarya1212 <aiswaryasivakumar84@gmail.com>
2023-07-31 08:30:48 +0000

    update

--- springapp/src/main/java/com/example/springapp/repository/Leaderboard.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.example.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springapp.model.Leaderboard;
-
-public interface LeaderboardRepository extends JpaRepository<Leaderboard,Long>{
-
-}
