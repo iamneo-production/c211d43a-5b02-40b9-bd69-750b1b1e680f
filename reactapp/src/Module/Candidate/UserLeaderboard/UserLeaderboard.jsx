2eb82934ddce68c577c1b63c3a70c6d84bc8926a
Rahulkumar246810 <rahulkumar246810@gmail.com>
2023-07-31 03:36:27 +0000

    update

--- reactapp/src/Module/Candidate/UserLeaderboard/UserLeaderboard.jsx
+++ reactapp/src/Module/Candidate/UserLeaderboard/UserLeaderboard.jsx
@@ -1,105 +1,98 @@
 import NavCandidate from '../../../Components/NavCandidate';
+//import React, { useEffect, useState } from "react";
 import './UserLeaderboard.css';
-import React, { useState } from 'react';
-import card from './cardData';
-import tabledata from './tableData';
+//import axios from "axios";
+//import base_url from "./api";
+import cardData from './cardData';
+import tableData from './tableData';
 
-function SearchBar({ onlSearch }) {
-  const handleSearchChange = (event) => {
-    const lsearchTerm = event.target.value;
-    onlSearch(lsearchTerm);
-  };
+
+export default function Leaderboard() {
+  
+/*const [Leaderboard , setLeaderboard] = useState([]);
+
+  const LoadData = () =>{
+    axios.get(`${base_url}/userleaderboard`).then(
+      (respose)=>{
+        console.log(respose);
+          setLeaderboard(respose.data);
+      },
+      (error)=>{
+        console.log(error);
+      }
+    )
+  }
+
+  useEffect(()=>{
+    LoadData();
+  },[])
+
+  const cardData = leaderboard.slice(0,3);
+  const tableData = leaderboard.slice(3); */
 
   return (
-    <div className="lsearch-bar">
-      <input
-        type="text"
-        placeholder="lSearch by name"
-        onChange={handleSearchChange}
-      />
-    </div>
-  );
-}
-  function Leaderboard() {
-    const [searchTerm, setSearchTerm] = useState('');
-  
-    const handleSearch = (searchTerm) => {
-      setSearchTerm(searchTerm);
-    };
-  
-    return (
-
-        <div>
-            <NavCandidate/>
-
-            <div className="App">
-      <div className="page_wrape">
-        {/* <!---main content--> */}
-
+    <div>
+      <NavCandidate/>
+      <div className="lApp">
+        <div className="lpage_wrape">
         <div className="lmain-content">
-          <div className="info-card">
-            {card &&
-              card.map((e, index) => (
-                <div className="card" key={index}>
-                  <div className="card-icon">
-                    <span>
-                      <i className="fa fa-trophy"></i>
-                    </span>
-                  </div>
-                  <div className="card-detail">
-                    <h2>{e.score}</h2>
-                    <h4>{e.name}</h4>
-                  </div>
-                  <p>{e.location}</p>
+        <div className="linfo-lcard">
+                {cardData.map((e,index) => {
+                    return (
+                        <div className="lcard" key={index}>
+                <div className="lcard-icon">
+                  <span>
+                    <i className="fa fa-trophy"></i>
+                  </span>
                 </div>
-              ))}
-          </div>
-        </div>
+                <div className="lcard-detail">
+                  <h2>{e.score}</h2>
+                  <h4>{e.name}</h4>
+                </div>
+                <p>{e.location}</p>
+              </div>
 
+                    );
+                })}
+            </div>
         <div className="lmain-table">
-          <div className="title">
-            <h2>All Users</h2>
-            <SearchBar onSearch={handleSearch} />
-            <div className="user_table">
-              <table>
-                <thead>
-                  <tr>
-                    <td>Names</td>
-                    <td>Score</td>
-                    <td>Email</td>
-                    <td>Location</td>
-                    <td>Greetings</td>
-                  </tr>
-                </thead>
-                <tbody>
-                  {tabledata &&
-                    tabledata
-                      .filter(
-                        (data) =>
-                          data.name.toLowerCase().includes(searchTerm.toLowerCase())
-                      )
-                      .map((tableData, j) => (
-                        <tr key={j}>
-                          <td>{tableData.name}</td>
-                          <td>{tableData.score}</td>
-                          <td>{tableData.email}</td>
-                          <td>{tableData.location}</td>
-                          <td>
-                            <a href="/#" className="btn">
-                              Congratulations
-                            </a>
-                          </td>
-                        </tr>
-                      ))}
-                </tbody>
-              </table>
+              <div className="title">
+                <h2>All Users</h2>
+                <div className="user_table">
+                  <table>
+                    <thead>
+                      <tr>
+                        <td>Names</td>
+                        <td>Score </td>
+                        <td>Email</td>
+                        <td>Location</td>
+                        <td>Greetings</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        {tableData.map((tabledata,j) =>{
+                                return (
+                                    <tr key={j}>
+                        <td>{tabledata.name}</td>
+                        <td>{tabledata.score}</td>
+                        <td>{tabledata.email}</td>
+                        <td>{tabledata.location}</td>
+                        <td>
+                          <a href="/#" className="btn">
+                            Congratulations
+                          </a>
+                        </td>
+                      </tr>
+                                );
+                        })}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
+      </div>
   );
 }
-
-export default Leaderboard;
